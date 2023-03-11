b5cafba46333489a8530cd6f8d8ce06a7537b5d7
Singroff <caleb.epps@yahoo.com>
2023-03-11 17:32:20 -0500

    - Added SSE Icon - Finalized Installer Layout - Added Skeleton for Export Window

---
+++ ExportWindow.py
@@ -0,0 +1,8 @@
+
+class ExportWindow:
+    pass
+
+
+
+if __name__ == "__main__":
+    pass
--- Installer.py
+++ Installer.py
@@ -5,7 +5,7 @@ from functools import partial
 
 from PySide2.QtCore import QSize, Qt
 from PySide2.QtCore import QThreadPool
-from PySide2.QtGui import QPixmap, QImage, QFont
+from PySide2.QtGui import QPixmap, QImage, QFont, QIcon
 from PySide2.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QGridLayout, QLabel, \
     QScrollArea, QHBoxLayout, QProgressBar, QProgressDialog, QVBoxLayout, QSizePolicy, QFileDialog
 
@@ -20,63 +20,82 @@ class Installer(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.setWindowTitle("SSE Installer ")
+        self.setWindowIcon(QIcon("assets/SSE.ico"))
+
         self.main_layout = QVBoxLayout()
         self.path_layout = QHBoxLayout()
+        self.path_layout.setAlignment(Qt.AlignCenter)
 
-        self.title = QLabel("Sing's Screenshot Editor")
+        self.title = QLabel("Steam Screenshot Editor")
         self.title.setFont(QFont('Arial', 24))
         self.title.setAlignment(Qt.AlignCenter)
 
-        self.select_path_inst_label = QLabel("Choose your Steam path")
-        self.select_path_inst_label.setAlignment(Qt.AlignCenter)
+        self.subtitle = QLabel("Developed by Sing")
+        self.subtitle.setFont(QFont('Arial', 16))
+        self.subtitle.setAlignment(Qt.AlignCenter)
 
-        self.get_steam_path_btn = QPushButton("Choose")
+
+        self.get_steam_path_btn = QPushButton("Select your Steam Path")
+        self.get_steam_path_btn.setFixedSize(300, 60)
+        self.get_steam_path_btn.setFont(QFont('Arial', 18))
         self.get_steam_path_btn.clicked.connect(self.on_select_steam_install_path_clicked)
+        self.path_layout.addWidget(self.get_steam_path_btn)
 
-        self.steam_path_label = QLabel()
-        self.steam_path_label.setAlignment(Qt.AlignCenter)
-        self.steam_path = None
-        self.steam_id = None
+        self.footer = QLabel("Open Sourced by Sing on Github.com")
+        self.footer.setFont(QFont('Arial', 8))
+        self.footer.setAlignment(Qt.AlignCenter)
 
-
-        # Populate QHBox
-        self.path_layout.addWidget(self.select_path_inst_label)
-        self.path_layout.addWidget(self.steam_path_label)
+        # self.steam_path_label = QLabel()
+        # self.steam_path_label.setAlignment(Qt.AlignCenter)
+        # self.steam_path = None
+        # self.steam_id = None
+        #
+        #
+        # # Populate QHBox
+        # self.path_layout.addWidget(self.select_path_inst_label)
+        # self.path_layout.addWidget(self.steam_path_label)
 
         # Populate Main Layout
         self.main_layout.addWidget(self.title)
+        self.main_layout.addWidget(self.subtitle)
         self.main_layout.addLayout(self.path_layout)
-        self.main_layout.addWidget(self.get_steam_path_btn)
-        self.setFixedSize(640, 480)
+        self.main_layout.addWidget(self.footer)
+        self.setFixedSize(400, 200)
         self.setLayout(self.main_layout)
         self.show()
 
     def on_select_steam_install_path_clicked(self):
-        self.steam_path = str(f"{QFileDialog.getExistingDirectory(self)}/userdata")
-        self.steam_id = os.listdir(self.steam_path)[0]
-        if self.steam_id == "0":
-            self.steam_id = os.listdir(self.steam_path[1])
-        print(self.steam_path)
-        print(self.steam_id)
+        self.steam_path = str(f"{QFileDialog.getExistingDirectory()}/userdata")
+        # simple print if no path selected
+        try:
+            self.steam_id = os.listdir(self.steam_path)[0]
+            if self.steam_id == "0":
+                self.steam_id = os.listdir(self.steam_path[1])
+            print(self.steam_path)
+            print(self.steam_id)
 
 
-        if not os.path.exists("cache"):
-            os.mkdir("cache")
-            os.mkdir("cache/image_cache")
+            if not os.path.exists("cache"):
+                os.mkdir("cache")
+                os.mkdir("cache/image_cache")
 
-        parser = ConfigParser()
-        parser["INFO"] = {
-            "path": f"{self.steam_path}/{self.steam_id}",
-            "export_path": "export/"
-        }
+            parser = ConfigParser()
+            parser["INFO"] = {
+                "path": f"{self.steam_path}/{self.steam_id}",
+                "export_path": "export/"
+            }
 
-        with open("cache/steam_info.config", 'w') as config:
-            parser.write(config)
+            with open("cache/steam_info.config", 'w') as config:
+                parser.write(config)
 
 
-        self.close()
+            self.close()
 
-        main_program = ScreenshotBrowser(steam_path=f"{self.steam_path}/{self.steam_id}")
+            ScreenshotBrowser(steam_path=f"{self.steam_path}/{self.steam_id}")
+
+        except FileNotFoundError:
+            print("No Path Selected")
 
 
 
